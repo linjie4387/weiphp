217c008e0b4bfb91b2a722e0c59b41f4fefaa6a8
linjie4387 <linjie4387@163.com>
2018-11-22 15:05:02 +0800

    添加jchx的配置

---
+++ Addons/Hospital/config_bak/config_jchx.php
@@ -0,0 +1,27 @@
+<?php
+return array (
+		"admin.server.url" => array (
+				"value" => "http://admin.chens.mobi/index.php/io/weiphp/updateuser" 
+		),
+		"juhe.api.url" => array (
+				"value" => "http://v.juhe.cn/sms/send" 
+		),
+		"juhe.api.key" => array (
+				"value" => "a967404d6cc941e9b13a2ce974226ec5" 
+		),
+		"sms.verify.switch" => array (
+				"value" => 'on' 
+		),
+		"template.token" => array(
+				"value" => 'gh_4e89dedf8a87'
+		),
+		"template.order.confirm" => array(
+				"value" => 'r6vvsDgC51xAyXpmh_ecDmflGJdADEd4ZGHnEJtguOs'
+		),
+		"template.delivery.confirm" => array(
+				"value" => '47Br0T2134hYGyfp1uWLIAvXt5lIHHYbtXiqsdicyAE'
+		),
+		"template.delivery.notify" => array(
+				"value" => 'BQ8gQHSt9wlLM_CzZWLLaNow9kbYr2MQNnKm4UOwouw'
+		) 
+);
